7a8218942455121b95ecc5a088fd5e192c9186af
monkeythemonkey <hamin.jin@education.nsw.gov.au>
2024-06-26 10:34:30 +1000

    Initial files for MakeCode project

---
+++ tutorial.custom.ts
@@ -0,0 +1,81 @@
+//% color=#b79900 icon="\uf1ce"
+namespace pizza {
+    // Make sure not to remove later player when earlier player tested
+    export let playersConnected=0;
+    //% blockId=set_players
+    //% block="set game for $num player(s) with $list"
+    //% num.defl=1
+    //% list.shadow=variables_get
+    //% list.defl=characters
+    export function setPlayersWith(list: Image[], num: number) {
+        if(num >= pizza.playersConnected){
+            pizza.playersConnected = num;
+            sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+            let xloc = [
+            40,
+            120,
+            40,
+            120
+            ]
+            let yloc = [
+            30,
+            30,
+            90,
+            90
+            ]
+            for (let index = 0; index <= num - 1; index++) {
+                mp.setPlayerSprite(mp.getPlayerByIndex(index), sprites.create(list[index], SpriteKind.Player))
+                mp.getPlayerSprite(mp.getPlayerByIndex(index)).setStayInScreen(true)
+                mp.getPlayerSprite(mp.getPlayerByIndex(index)).setPosition(xloc.shift(), yloc.shift())
+                mp.getPlayerSprite(mp.getPlayerByIndex(index)).z = 1000
+                mp.moveWithButtons(mp.getPlayerByIndex(index))
+            }
+        }
+    }
+    //% blockId=bump_sprite
+    //% block="$thisSprite bump $thatSprite"
+    //% thisSprite.shadow=variables_get
+    //% thisSprite.defl=sprite
+    //% thatSprite.shadow=variables_get
+    //% thatSprite.defl=otherSprite
+    export function bumpSprite(thisSprite: Sprite, thatSprite: Sprite) {
+        thatSprite.setPosition((thisSprite.x + 80) % 160, thisSprite.y)
+    }
+}
+namespace logic{
+    /**
+    * this just holds code
+    */
+    //% block="check if A button pressed"  weight=300
+    //% handlerStatement=1
+    export function wrap1(handler: () => void) {
+        handler();
+    }
+}
+//% color=#6d5ba4 icon="\uf0f2"
+namespace bundles{
+    /**
+    * this just holds code
+    */
+    //% block="check if A button pressed"  weight=300
+    //% handlerStatement=1
+    export function wrap1(handler: () => void) {
+        handler();
+    }
+    /**
+    * this just holds code
+    */
+    //% block="create finish line"  weight=300
+    //% handlerStatement=1
+    export function wrap2(handler: () => void) {
+        handler();
+    }
+    /**
+    * this just holds code
+    */
+    //% block="create players"  weight=300
+    //% handlerStatement=1
+    export function wrap3(handler: () => void) {
+        handler();
+    }
+}
